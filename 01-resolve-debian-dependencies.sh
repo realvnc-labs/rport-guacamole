5e506c27dae1adfe9438f2ced9c6d61858165e50
Thorsten Kramm <tk@system42.io>
2022-02-06 17:38:01 +0100

    Fixed bionic dependencies

--- 01-resolve-debian-dependencies.sh
+++ 01-resolve-debian-dependencies.sh
@@ -12,8 +12,13 @@ else
     # Debian package name
     LIB_JPEG="libjpeg62-turbo-dev"
 fi
+if [ $VERSION_CODENAME = "bionic" ];then
+    EXTRA_PKGS="libossp-uuid-dev"
+else
+    EXTRA_PKGS=''
+fi
 DEBIAN_FRONTEND=noninteractive apt-get -y install ${LIB_JPEG} libcairo2-dev libpng-dev libtool-bin \
  libavcodec-dev libavformat-dev libavutil-dev \
  freerdp2-dev libpango1.0-dev libssh2-1-dev libtelnet-dev \
- libvncserver-dev libpulse-dev libssl-dev \
- libvorbis-dev libwebp-dev libwebsockets-dev make curl
+ libvncserver-dev libpulse-dev libssl-dev libuuid1 \
+ libvorbis-dev libwebp-dev libwebsockets-dev make curl ${EXTRA_PKGS}
