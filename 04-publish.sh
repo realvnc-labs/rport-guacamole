861a39db7e91ac66d088bc31ca8136e95f45f87e
Thorsten Kramm <tk@system42.io>
2022-02-06 15:42:34 +0100

    Fixed upload source

--- 04-publish.sh
+++ 04-publish.sh
@@ -1,4 +1,7 @@
+set -e
 UPLOAD=$1
+cd /
+ls -lah
 curl -f -u ${BITBUCKET_USERNAME}:${BITBUCKET_APP_PASSWORD} \
 "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/downloads" \
 -F files=@{$UPLOAD}
