5a21a297ed26393b84e895075b9be8807968583e
Thorsten Kramm <tk@system42.io>
2022-02-15 22:52:34 +0100

    linintian for Debian Bullseye

--- 04-lintian.sh
+++ 04-lintian.sh
@@ -1,2 +1,11 @@
-DEBIAN_FRONTEND=noninteractive apt-get -y install lintian
-lintian -X files/hierarchy-standard /rport-guacamole_1.4.0_ubuntu_focal_x86_64.deb
+DEBIAN_FRONTEND=noninteractive apt-get -y install lintian >/dev/null
+. /etc/os-release
+DEB_FILE=/rport-guacamole_1.4.0_debian_bullseye_amd64.deb
+case $VERSION_CODENAME in
+    bullseye)
+        lintian -X files/hierarchy/standard $DEB_FILE
+        ;;
+    focal)
+        lintian -X files/hierarchy-standard $DEB_FILE
+        ;;
+esac
